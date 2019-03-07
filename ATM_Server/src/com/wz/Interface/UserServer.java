0361c40bb8d27adf87d1ccc3b35a7751ec079fbe
ReWangZheng <29370330@qq.com>
2019-03-07 21:29:24 +0800

    修复了一点点接口问题的小BUG

--- ATM_Server/src/com/wz/Interface/UserServer.java
+++ ATM_Server/src/com/wz/Interface/UserServer.java
@@ -8,5 +8,5 @@ public interface UserServer {
 	public boolean drawMoney(User user,double money); //取钱，如果余额足够则取钱成功，如果余额不足取钱，则返回false
 	public boolean transfer(User from,String to,double money); //转账，如果转账成功则取返回true，如果失败则返回false
 	public void saveMoney(User user,double money); //存钱
-	public void updatePasswd(User user,String newpasswd);
+	public boolean updatePasswd(User user,String newpasswd);
 }
